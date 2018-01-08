47cb62b4a8e2aa5571f547408a05ba81bf7e665f
Oluf Lorenzen <lorenzen@hornetsecurity.com>
2018-01-08 09:32:37 +0100

    add script to set screen config

---
+++ .config/i3/screenset.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eu
+
+_CONNECTED_SCREENS=$(xrandr | grep '\Wconnected' | awk '{ print $1 }')
+
+if [[ $_CONNECTED_SCREENS =~ ^HDMI.*$ ]]; then
+    ~/.screenlayout/home.sh
+elif [[ $_CONNECTED_SCREENS =~ ^DP1-1*$ ]]; then
+    ~/.screenlayout/work.sh
+fi
