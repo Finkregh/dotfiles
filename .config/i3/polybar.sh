3d7b164b0042f79a387820b6e4e4f68f77fd4307
Oluf Lorenzen <lorenzen@hornetsecurity.com>
2018-01-08 09:37:52 +0100

    polybar: set config depending on connected screen

--- .config/i3/polybar.sh
+++ .config/i3/polybar.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+_CONNECTED_SCREENS=$(xrandr | grep '\Wconnected' | awk '{ print $1 }')
+
+if [[ $_CONNECTED_SCREENS =~ ^HDMI1$ ]]; then
+    export MONITOR=HDMI1
+elif [[ $_CONNECTED_SCREENS =~ ^DP1-1$ ]]; then
+    export MONITOR=DP1-1
+fi
+
 # Terminate already running bar instances
 killall -q polybar
 
