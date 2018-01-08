8bef0f4243453796fa5fb25a9ae2afdd39b94e3e
Oluf Lorenzen <lorenzen@hornetsecurity.com>
2018-01-08 09:40:48 +0100

    m: copypasta matching error

--- .config/i3/screenset.sh
+++ .config/i3/screenset.sh
@@ -4,8 +4,8 @@ set -eu
 
 _CONNECTED_SCREENS=$(xrandr | grep '\Wconnected' | awk '{ print $1 }')
 
-if [[ $_CONNECTED_SCREENS =~ ^HDMI.*$ ]]; then
+if [[ $_CONNECTED_SCREENS =~ HDMI1 ]]; then
     ~/.screenlayout/home.sh
-elif [[ $_CONNECTED_SCREENS =~ ^DP1-1*$ ]]; then
+elif [[ $_CONNECTED_SCREENS =~ DP1-1 ]]; then
     ~/.screenlayout/work.sh
 fi
