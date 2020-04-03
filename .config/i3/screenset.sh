5fa0dbe6b66271f6f29981ed203137680dbcf321
Oluf Lorenzen <oluf.lorenzen@b1-systems.de>
2020-04-03 21:28:49 +0200

    i3,polybar: update screen config

--- .config/i3/screenset.sh
+++ .config/i3/screenset.sh
@@ -6,6 +6,6 @@ _CONNECTED_SCREENS=$(xrandr | grep '\Wconnected' | awk '{ print $1 }')
 
 if [[ $_CONNECTED_SCREENS =~ HDMI1 ]]; then
     ~/.screenlayout/home.sh
-elif [[ $_CONNECTED_SCREENS =~ DP1-1 ]]; then
+elif [[ $_CONNECTED_SCREENS =~ DP-1-1 ]]; then
     ~/.screenlayout/work.sh
 fi
